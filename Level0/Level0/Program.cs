69a836f5761d4035b2c757f5dbba0f3e396926cd
WujuSan <axel.breno@hotmail.com>
2016-01-06 18:28:13 -0200

    Added GitHub check, now we'll start to change the AIO version

--- Level0/Level0/Program.cs
+++ Level0/Level0/Program.cs
@@ -1,12 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
+using System.Net;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+using System.Reflection;
 using EloBuddy;
-using EloBuddy.SDK;
 using EloBuddy.SDK.Events;
-using EloBuddy.SDK.Menu;
 using LevelZero.Model;
-using LevelZero.Model.Values;
 using LevelZero.Util;
 using SharpDX;
 
@@ -21,6 +20,32 @@ namespace LevelZero
 
         private static void GameLoaded(EventArgs args)
         {
+            Task.Factory.StartNew(() =>
+            {
+                try
+                {
+                    var AssVersion = Assembly.GetExecutingAssembly().GetName().Version;
+
+                    string Text = new WebClient().DownloadString("https://raw.githubusercontent.com/mrarticuno/Elobuddy/master/Level0/Level0/Properties/AssemblyInfo.cs");
+
+                    var Match = new Regex(@"\[assembly\: AssemblyVersion\(""(\d{1,})\.(\d{1,})\.(\d{1,})\.(\d{1,})""\)\]").Match(Text);
+
+                    if (Match.Success)
+                    {
+                        var CorrectVersion = new System.Version(string.Format("{0}.{1}.{2}.{3}", Match.Groups[1], Match.Groups[2], Match.Groups[3], Match.Groups[4]));
+
+                        if (CorrectVersion > AssVersion)
+                        {
+                            NotificationUtil.DrawNotification(new NotificationModel(Game.Time, 20f, 1f, "Your AIO version is OUTDATED, current version is " + CorrectVersion, Color.Yellow));
+                        }
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e + "\n [ [RIP] Search ]");
+                }
+            });
+
             try
             {
                 var handle = Activator.CreateInstance(null, "LevelZero.Core.Champions." + Player.Instance.ChampionName);

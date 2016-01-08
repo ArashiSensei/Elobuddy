fd900b024929fe0bd6e9a443aa47bca606b3d6e6
WujuSan <axel.breno@hotmail.com>
2016-01-08 13:11:27 -0200

    Added single damage spell calculation

--- Level0/Level0/Util/DamageUtil.cs
+++ Level0/Level0/Util/DamageUtil.cs
@@ -10,10 +10,10 @@ namespace LevelZero.Util
     {
         public static List<SpellDamage> SpellsDamage { get; set; }
 
-         public static float GetComboDamage(Obj_AI_Base enemy)
-         {
-             return SpellsDamage.Where(spellBase => spellBase.Spell.IsReady()).Sum(spellBase => Player.Instance.CalculateDamageOnUnit(enemy, spellBase.DamageType, spellBase.SpellDamageValue[spellBase.Spell.Level] + spellBase.SpellDamageModifier[spellBase.Spell.Level] * (spellBase.DamageType == DamageType.Magical ? Player.Instance.FlatMagicDamageMod : Player.Instance.FlatPhysicalDamageMod)));
-         }
+        public static float GetComboDamage(Obj_AI_Base enemy)
+        {
+            return SpellsDamage.Where(spellBase => spellBase.Spell.IsReady()).Sum(spellBase => Player.Instance.CalculateDamageOnUnit(enemy, spellBase.DamageType, spellBase.SpellDamageValue[spellBase.Spell.Level] + spellBase.SpellDamageModifier[spellBase.Spell.Level] * (spellBase.DamageType == DamageType.Magical ? Player.Instance.FlatMagicDamageMod : Player.Instance.FlatPhysicalDamageMod)));
+        }
 
         public static bool Killable(Obj_AI_Base enemy, SpellSlot slot, float damageDecrease = 0)
         {
@@ -25,13 +25,35 @@ namespace LevelZero.Util
                     ? Player.Instance.FlatMagicDamageMod
                     : Player.Instance.FlatPhysicalDamageMod));
 
-            if (spellBase.DamageType == DamageType.True)
+            return enemy.Health < (possibleDamage - damageDecrease);
+        }
+
+        public static float GetSpellDamage(SpellSlot slot, Obj_AI_Base enemy, bool IsAbility = true, bool IsAAOrTargetted = false)
+        {
+            float damage = new float();
+
+            foreach (var dmg in SpellsDamage.Where(it => it.Spell.Slot == slot))
             {
-                return enemy.Health < (spellBase.SpellDamageValue[spellBase.Spell.Level] +
-                spellBase.SpellDamageModifier[spellBase.Spell.Level] * Player.Instance.FlatPhysicalDamageMod - damageDecrease);
+                if (dmg.DamageType == DamageType.Magical)
+                {
+                    damage += Player.Instance.CalculateDamageOnUnit(enemy, dmg.DamageType, dmg.SpellDamageValue[dmg.Spell.Level] + dmg.SpellDamageModifier[dmg.Spell.Level] * Player.Instance.FlatMagicDamageMod, IsAbility, IsAAOrTargetted);
+                    continue;
+                }
+
+                if (dmg.DamageType == DamageType.Physical)
+                {
+                    damage += Player.Instance.CalculateDamageOnUnit(enemy, dmg.DamageType, dmg.SpellDamageValue[dmg.Spell.Level] + dmg.SpellDamageModifier[dmg.Spell.Level] * Player.Instance.FlatPhysicalDamageMod, IsAbility, IsAAOrTargetted);
+                    continue;
+                }
+
+                if (dmg.DamageType == DamageType.True)
+                {
+                    damage += Player.Instance.CalculateDamageOnUnit(enemy, dmg.DamageType, dmg.SpellDamageValue[dmg.Spell.Level] + dmg.SpellDamageModifier[dmg.Spell.Level] * Player.Instance.FlatPhysicalDamageMod, IsAbility, IsAAOrTargetted);
+                    continue;
+                }
             }
 
-            return enemy.Health < (possibleDamage - damageDecrease);
+            return damage;
         }
     }
 }
